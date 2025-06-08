1ef2e35dc544d3a73f28bd574cdeb8c1a41a2fbd
bikal111 <bk2bikal@gmail.com>
2025-06-08 08:22:17 +0545

    List of doctors is displayed including the routes of individual doctors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors:{
         'primary':"#189d01"
+      },
+      gridTemplateColumns:{
+        'auto':'repeat(auto-fill, minmax(200px, 1fr))'
       }
     },
   },

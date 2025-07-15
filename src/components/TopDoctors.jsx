6a4aa187fb12cd3791f1ae887460d9cae403d85a
bikal111 <bk2bikal@gmail.com>
2025-07-15 20:40:43 +0545

    related doc

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -11,7 +11,7 @@ const TopDoctors = () => {
         <p className='sm:w-1/2 text-center text-sm '>Easily browse our extensive list of trusted doctors and book with confidence.</p>
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
             {doctors.slice(0,10).map((item,index)=>(
-                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 ' key={index}>
+                <div  onClick={()=>{navigate(`/appointment/${item._id}`) ;scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 ' key={index}>
                     <img className='bg-blue-50' src={item.image} alt="" srcset="" />
                     <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-center text-green-500  '>

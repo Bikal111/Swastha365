17a913592a491a4d573226587fdf61c7b7d65aa0
bikal111 <bk2bikal@gmail.com>
2025-08-01 22:51:17 +0545

    admin authentication using password and email using api but still left

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,8 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from "cloudinary"
 import doctorModel from '../models/docotorModel.js'
+import jwt from 'jsonwebtoken'
+
 // API FOR Adding doc
 const addDoctor = async(req,res) =>{
     try {
@@ -70,4 +72,27 @@ const addDoctor = async(req,res) =>{
     }
 }
 
-export{addDoctor}
+//Api for admin login
+
+const loginAdmin = async (req,res) =>{
+    try {
+        const {email,password} = req.body
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
+
+
+        } else{
+            res.json({success:false,message:"Invalid Password or Email "})
+        }
+
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+}
+
+export{addDoctor,loginAdmin}

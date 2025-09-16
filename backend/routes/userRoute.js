d40cd3813801f2292c8a9fafbec3907ec8a1999c
bikal111 <bk2bikal@gmail.com>
2025-09-16 22:15:47 +0545

    bokking of doctors from patient

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser,loginUser, getProfile , updateProfile } from '../controllers/userController.js'
+import { registerUser,loginUser, getProfile , updateProfile,bookAppointment,listAppointment } from '../controllers/userController.js'
 import authUser from '../middleweares/authUser.js'
 import upload from '../middleweares/multer.js'
 
@@ -9,6 +9,8 @@ userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 userRouter.get('/get-profile',authUser,getProfile)
 userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
+userRouter.post('/book-appointment',authUser,bookAppointment)
+userRouter.get('/appointments',authUser,listAppointment)
 
 
 
